2467c4b8d6060e529fa8c7a9e23c22efb507d8fd
Asad Khan <asad_khan47@hotmail.co.uk>
2019-04-11 16:59:41 +0100

    Necessary change to make the code the same as the source code, local host now shows true board

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="Square">
+      <button className="square">
         {/* TODO */}
       </button>
     );
